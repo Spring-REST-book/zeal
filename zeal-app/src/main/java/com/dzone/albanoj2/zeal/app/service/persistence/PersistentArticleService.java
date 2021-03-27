577e0f0ef98ee0760795bd407a68b4a9674edc36
Justin Albano <albano.justin@gmail.com>
2021-03-27 11:06:15 -0400

    Added zeal-app module to parent POM.

---
+++ zeal-app/src/main/java/com/dzone/albanoj2/zeal/app/service/persistence/PersistentArticleService.java
@@ -0,0 +1,34 @@
+package com.dzone.albanoj2.zeal.app.service.persistence;
+
+import java.util.List;
+
+import com.dzone.albanoj2.zeal.app.service.ArticleService;
+import com.dzone.albanoj2.zeal.domain.Article;
+
+public class PersistentArticleService implements ArticleService {
+
+	@Override
+	public List<Article> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Article findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Article save(Article article) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
